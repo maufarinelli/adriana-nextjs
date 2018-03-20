9d48c170a49dfbe80aeb0478303bcab977085261
Mauricio Farinelli <mauricio@sweetiq.com>
2018-03-20 17:12:20 -0400

    Using getInitialProps to preload content on the server side

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,16 @@
-import { Component } from 'react';
+import React from 'react';
 import fetchSamplePage from '../services/fetchContent';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      page: {}
-    };
-  }
-
-  componentWillMount() {
-    this.fetchPage();
-  }
-
-  fetchPage() {
-    const page = fetchSamplePage();
-
-    page.then(response => this.setState({page: response}));
+class App extends React.Component {
+  static async getInitialProps() {
+    const page = await fetchSamplePage();
+    const pageJson = await page.json();
+    return { pageJson };
   }
 
   render() {
-    const content = this.state.page.content && this.state.page.content.rendered;
+    const { pageJson } = this.props;
+    const content = pageJson.content.rendered;
 
     return (
       <div>
--- services/fetchContent.js
+++ services/fetchContent.js
@@ -3,6 +3,5 @@ import fetch from 'node-fetch';
 export default function fetchSamplePage() {
   const url = 'http://159.89.116.112/adriana/wp-json/wp/v2/pages/2';
 
-  return fetch(url)
-    .then(response => response.json());
+  return fetch(url);
 }
